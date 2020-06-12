e1ca43f3b788f1efd8dccddba25c861e0f67b24b
Arapaut V Sivaprasad <avs@webgenie.com>
2020-06-12 18:56:24 +1000

    Update eighteen_mail_opened_check.pl
    First working version. Records and mails details.

--- eighteen_mail_opened_check.pl
+++ eighteen_mail_opened_check.pl
@@ -7,24 +7,199 @@
 #------------------------------------------------------------------------------
 
 require "/var/www/vhosts/webgenie.com/cgi-bin/debug.pl";
+use Time::Piece;
+use DBI;
+use Date::Parse;
+use DateTime;
 
-sub ReadTheAccessLog
+sub TimeDiff
 {
-	open (INP, "<$accesslog");
-	@filecontent = <INP>;
-	close(INP);
-	$len = $#filecontent;
+	my $t1 = $_[0];
+	my $t2 = $_[1];
+#	$t1 = 'Jun/12/2020:13:20:45';
+#	$t2 = 'Jun/12/2020:13:26:04';
+	my $diff = str2time($t2) - str2time($t1);
+	return $diff;
+}
+
+sub ConnectToDBase
+{
+$driver = "mysql";
+$hostname = "localhost";
+$database = "mailopened"; 
+$dbuser="avs";
+$dbpassword='2Kenooch';
+   $dsn = "DBI:$driver:database=$database;host=$hostname";
+   $dbh = DBI->connect($dsn, $dbuser, $dbpassword);
+   $drh = DBI->install_driver("mysql");
+}
+
+sub execute_query
+{
+	$sth=$dbh->prepare($query);
+	$rv = $sth->execute or die "can't execute the query: $sth->errstr";
+}
+
+sub Fetchrow_array
+{
+   $tablerows = $_[0];
+   my @results = ();
+   while(@entries = $sth->fetchrow_array)
+   {
+	   for ($jj=0; $jj < $tablerows; $jj++)
+	   {
+		   push (@results, $entries[$jj]);
+	   }
+   }
+   my $numreturned = scalar(@results)/$tablerows;
+   return @results;
+}
+
+sub SendMailToOwner
+{
+	my $Owner_name = "Mail Delivery Monitor";
+	my $Owner_email = 'avs2904@webgenie.com';
+	$tfilecontent =  "From:$Owner_name <$Owner_email>\n";
+	$tfilecontent .=  "To:$Owner_name <$Owner_email>\n";
+	$tfilecontent .= "MIME-Version: 1.0\n";                                 # Inactivate this line to send text-only msg
+	$tfilecontent .= "Subject:$subject\n";
+	$tfilecontent .= "Content-Type: text/html; charset=ISO-8859-1; format=flowed\n";
+	$tfilecontent .= "Content-Transfer-Encoding: 7bit\n\n";
+	$tfilecontent .= $content;
+
+	my $filename = "$tmpDir/hn_$$.tmp";
+	open (OUT, ">$filename");
+	print OUT $tfilecontent;
+	close (OUT);
+	`$mailprogram -t < $filename`;
+	$mailerror = $?;
+	if(!$mailerror)
+	{
+		unlink($filename);
+	}
+}
+sub AddToDatabase
+{
+	&ConnectToDBase;
+	#select user_id, rand, to_address, ip, opened_time from mails where user_id='avs123456789' and rand=123456781 and to_address='avs_webgenie_com@me.com' and ip='49.195.91.51' and status=1;
+	$query = "select user_id, to_address, opened_time from mails where user_id='$this_user_id' and rand=$this_rand and to_address='$this_address' and ip='$this_ip' and status=1 limit 0,1";
+#	&d($query);
+	&execute_query($query);
+	my @results = &Fetchrow_array(3);
+	$user_id = $results[0];
+#	$rand = $results[1];
+	$to_address = $results[1];
+#	$ip = $results[3];
+	$mailed_time = $results[2];
+#&debug("query = $query; perc = $perc");
+	if (!$user_id)
+	{
+		#insert into `mails` (user_id, rand, to_address, opened_time, ip, sent_time) values ('avs123456789', 123456781, 'avs_webgenie_com@me.com', 'Jun/12/2020:13:20:45', '49.195.91.51', true)
+		$query = "insert into `mails` (user_id, rand, to_address, opened_time, ip, sent_time) values ('$this_user_id', '$this_rand', '$this_address', '$this_time', '$this_ip', true)";
+#		&d($query);
+		&execute_query($query);
+	}
+	else
+	{
+		$diff = &TimeDiff($mailed_time, $this_time);
+#		&d("diff=$diff");
+		if ($diff > 30)
+		{
+			$query = "select `id` from `mails` where user_id='$this_user_id' and rand=$this_rand and to_address='$this_address' and ip='$this_ip' and opened_time='$this_time' and status=1 limit 0,1";
+#			&d($query);
+			&execute_query($query);
+			my @results = &Fetchrow_array(1);
+			$id = $results[0];
+			if (!$id)
+			{
+				$query = "select `user_email` from `users` where user_id='$this_user_id' and status=1 limit 0,1";
+				&execute_query($query);
+				my @results = &Fetchrow_array(1);
+				$user_email = $results[0];
+				$query = "insert into `mails` (user_id, rand, to_address, opened_time, ip, sent_time) values ('$this_user_id', '$this_rand', '$this_address', '$this_time', '$this_ip', true)";
+				&execute_query($query);
+				$subject = "Mail Opened by $this_address";
+				$content = "
+				<table cellspacing=\"1\" style=\"width: 400px; border-left-style: solid; border-left-width: 1px; border-right: 1px solid #C0C0C0; border-top-style: solid; border-top-width: 1px; border-bottom: 1px solid #C0C0C0\">
+					<tr>
+						<td>Sender ID:&nbsp;</td>
+						<td>$user_id&nbsp;</td>
+					</tr>
+					<tr>
+						<td>Sender Email:&nbsp;</td>
+						<td>$user_email&nbsp;</td>
+					</tr>
+					<tr>
+						<td>Mail sent time:&nbsp;</td>
+						<td>$mailed_time&nbsp;</td>
+					</tr>
+					<tr>
+						<td>Sent to:&nbsp;</td>
+						<td>$to_address&nbsp;</td>
+					</tr>
+					<tr>
+						<td>Mail opened time:&nbsp;</td>
+						<td>$this_time&nbsp;</td>
+					</tr>
+					<tr>
+						<td>IP address opened from:&nbsp;</td>
+						<td>$this_ip&nbsp;</td>
+					</tr>
+				</table>
+				";
+				&d("Sending mail");
+				&SendMailToOwner;
+			}
+		}
+#		&d("ip=$ip; this_ip=$this_ip, opened_time=$opened_time; this_time=$this_time; user_id=$user_id; this_user_id=$this_user_id; rand=$rand; this_rand=$this_rand; to_address=$to_address; this_address=$this_address");
+	}
+	$dbh->disconnect;
 }
 
 sub FindTheSentLine
 {
+	$logLines = `grep "GET /img/mail/" /var/www/vhosts/system/webgenie.com/logs/access_ssl_log`;
+	@logLines = split(/\n/, $logLines);
+	$len = $#logLines;
+#	&d("$len. @logLines");
+	for (my $j=0; $j <= $len; $j++)
+	{
+#		&d("$j. $logLines[$j]");
+		$logLines[$j] =~ tr/  / /s;
+		@fields = split(/ /, $logLines[$j]);
+		$this_ip = $fields[0];
+		$time = $fields[3]; #[12/Jun/2020:08:59:51
+		$img = $fields[6];
+#		&d("$ip; $time; $img");
+	
+		# Convert the time to a parsable format as Month/Date/Year:Hour:Min:Sec. e.g Jun/12/2020:13:20:45
+		@fields = split(/:/, $time); # [12/Jun/2020:13:20:45
+		$time = "$fields[1]:$fields[2]:$fields[3]"; # Time part. 13:20:45
+
+		@fields = split(/\//, $fields[0]); # Date part. [12/Jun/2020
+		$fields[0] =~ s/\[//g; # 12
+		$date = "$fields[1]/$fields[0]/$fields[2]"; # Jun/12/2020
+		
+		$this_time = "$date:$time"; # Jun/12/2020:13:20:45
+#		&d("$ip; $time; $img");
+
+		# Proceed only if the img part is in the right format
+		if ($img =~ /\/img\/mail\/(.*)\.png\?(\d*)\+(.*)/) # /img/mail/avs123456789.png?123456781+avs_webgenie_com@me.com
+		{
+			$this_user_id = $1;
+			$this_rand = $2;
+			$this_address = $3;
+			&AddToDatabase;
+		}
+	}
 }
 
 sub do_main
 {	
-	&ReadTheAccessLog;
+	&TimeDiff;
+#	&ReadTheAccessLog;
 	&FindTheSentLine;
-	&CheckMailOpened;
+#	&CheckMailOpened;
 }
 $ProcessTime = `/bin/date`; $ProcessTime =~ s/\n//g ;
 $accesslog = "/var/www/vhosts/system/webgenie.com/logs/access_ssl_log";
@@ -37,5 +212,9 @@ $from_address = 'avs2904@webgenie.com';
 $|=1;
 &do_main;
 sleep(1);
-#CREATE TABLE `mails` (`id` int(4) NOT NULL AUTO_INCREMENT,  `user_id` varchar(30) NOT NULL DEFAULT '', `recipient` varchar(50) NOT NULL DEFAULT '', `sent_time` varchar(30) NOT NULL DEFAULT '', `opened_time` varchar(30) NOT NULL DEFAULT '',  `ip` varchar(15) NOT NULL DEFAULT '', `status` int(1) DEFAULT '1',  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,  PRIMARY KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+#CREATE TABLE `users` (`id` int(4) NOT NULL AUTO_INCREMENT,  `user_id` varchar(30) NOT NULL DEFAULT '', `user_email` varchar(30) NOT NULL DEFAULT '', `status` int(1) DEFAULT '1',  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`user_id`), KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+#CREATE TABLE `mails` (`id` int(4) NOT NULL AUTO_INCREMENT,  `user_id` varchar(30) NOT NULL DEFAULT '', `rand` varchar(30) NOT NULL DEFAULT '', `to_address` varchar(50) NOT NULL DEFAULT '', `sent_time` bool default null, `opened_time` varchar(30) NOT NULL DEFAULT '',  `ip` varchar(15) NOT NULL DEFAULT '', `status` int(1) DEFAULT '1',  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+#insert into `users` (user_id, user_email) values ('avs123456789', 'avs2904@webgenie.com');
+#insert into `mails` (user_id, rand, to_address, opened_time, ip) values ('avs123456789', '123456781', 'avs_webgenie_com@me.com', 'Jun/12/2020:13:20:45', '49.195.91.51');
 
