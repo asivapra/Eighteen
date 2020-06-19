8c455168c0427e78dd879fd7aa6e9fbfb641d35d
Arapaut V Sivaprasad <avs@webgenie.com>
2020-06-19 14:03:36 +1000

    First commit of MOC files
    These are the web app for managing the user ID and addresses for MOC

---
+++ MOC/cgi-bin/moc.cgi
@@ -0,0 +1,174 @@
+#!/usr/bin/env perl
+# Created on 15 Jun, 2020
+# Last edit: 15 Jun, 2020
+# By Dr. Arapaut V. Sivaprasad
+=pod
+This CGI is for creating the Image URL for 'Mail Opened Check'
+=cut
+# -----------------------------------
+require "/var/www/vhosts/webgenie.com/cgi-bin/debug.pl";
+use DBI;
+sub reformat
+{
+  local($tmp) = $_[0] ;
+  $tmp =~ s/\+/ /g ;
+  while ($tmp =~ /%([0-9A-Fa-f][0-9A-Fa-f])/)
+  {
+   $num = $1;
+   $dec = hex($num);
+   $chr = pack("c",$dec);
+   $chr =~ s/&/and/g;  # Replace if it is the & char.
+   $tmp =~ s/%$num/$chr/g;
+  }
+  return($tmp);
+}
+sub Get_fields
+{
+   my @pquery = split(/\&/, $pquery);
+   for my $item (@pquery)
+   {
+           if ($item =~ /(.*)=(.*)/i)
+           {
+                $$1 = $2; 
+           }
+   }
+}
+
+sub ConnectToDBase
+{
+$driver = "mysql";
+$hostname = "localhost";
+$database = "mailopened"; 
+$dbuser="avs";
+$dbpassword='2Kenooch';
+   $dsn = "DBI:$driver:database=$database;host=$hostname";
+   $dbh = DBI->connect($dsn, $dbuser, $dbpassword);
+   $drh = DBI->install_driver("mysql");
+}
+
+sub execute_query
+{
+	$sth=$dbh->prepare($query);
+	$rv = $sth->execute or die "can't execute the query: $sth->errstr";
+}
+
+sub Fetchrow_array
+{
+   $tablerows = $_[0];
+   my @results = ();
+   while(@entries = $sth->fetchrow_array)
+   {
+	   for ($jj=0; $jj < $tablerows; $jj++)
+	   {
+		   push (@results, $entries[$jj]);
+	   }
+   }
+   my $numreturned = scalar(@results)/$tablerows;
+   return @results;
+}
+
+sub GetRandomChars
+{
+        $n = $_[0];
+        $alphanumeric = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        @alphanumeric = split (//, $alphanumeric);
+        $randomString = "";
+        for (my $j=0; $j <= $n; $j++)
+        {
+                srand;  # Seed the random number
+                $i = int (rand (62));  # Get a random start position
+                $randomString .= $alphanumeric[$i];
+        }
+        return $randomString;
+}
+
+sub CreateImageLink
+{
+	my $to_address = $_[0];
+	$random = &GetRandomChars(9);
+	my $img_url = $baseURL . "?" . $random . "+" . $to_address;
+	return $img_url;
+}
+
+sub do_main
+{
+#&debugEnv;	
+	# Kill a runaway CGI, if any.
+	my $psline = `ps -ef | grep moc.cgi | grep -v grep`;
+	my @fields = split (/\s/, $psline);
+	$pid = $fields[1];
+	my $thispid = $$;
+	if ($pid && $pid ne $thispid) { `kill $pid`; }
+
+	my $cl = $ENV{'CONTENT_LENGTH'}; # Method=POST will have a non-zero value.
+	$cl //= 0; # Set the value as 0 if $cl is undefined. It won't happen on a well built Apache server.
+	if ($cl > 0)
+	{
+		read(STDIN, $_, $cl);
+		$_ .= "&"; # Append an & char so that the last item is not ignored
+		$pquery = &reformat($_);
+		print "Content-type: text/html\n\n"; $headerAdded = 1;
+	}
+	else
+	{
+		$sc_action = $ARGV[0];
+		&Get_fields;	# Parse the $pquery to get all form input values
+		if (!$sc_action)
+		{
+			$dumb = 1; # This is a dumb URL with &BBOX=0,0,0,0 added at the end.
+			$request_string = $ENV{QUERY_STRING};
+			@fields = split (/\&/, $request_string);
+			$sc_action = $fields[0];
+			@fields = split (/\+/, $sc_action);
+			$sc_action = $fields[0];
+		}
+		if ($sc_action eq "MOC")
+		{
+			print "Content-type: text/html\n\n"; $headerAdded = 1;
+			$pquery = reformat($ARGV[2]);
+			$pquery =~ s/\\//gi;
+			&Get_fields;	# Parse the $pquery to get all form input values
+			&Get_Cookie;
+			$img_url = &CreateImageLink($to_address);
+			print "$img_url\n";
+			exit;
+		}
+		if ($sc_action eq "login")
+		{
+			$User_email = $ARGV[2];
+			$Password = $ARGV[3];
+			print "Content-type: text/html\n\n"; $headerAdded = 1;
+			&ConnectToDBase;
+			$query = "select user_id from `users` where user_email='$User_email' and password='$Password' limit 0,1";
+			&execute_query($query);
+			my @results = &Fetchrow_array(1);
+			$user_id = $results[0];
+			$dbh->disconnect;
+			if ($user_id)
+			{
+				print $user_id; 
+			}
+			else
+			{
+				print "";
+			}
+		}
+		else
+		{
+			&debug("Warning: sc_action not found!");
+		}
+	}
+}
+$|=1;
+$ct0 = time();
+$baseURL = "https://www.webgenie.com/img/mail/avs123456789.png";
+$ProcessTime = `/bin/date`; $ProcessTime =~ s/\n//g ;
+&do_main;
+#https://www.webgenie.com/img/mail/avs123456789.png?lZImV8ZRbP+avs_webgenie_com@me.com
+#CREATE TABLE `mails` (`id` int(4) NOT NULL AUTO_INCREMENT,  `user_id` varchar(30) NOT NULL DEFAULT '', `rand` varchar(30) NOT NULL DEFAULT '', `to_address` varchar(50) NOT NULL DEFAULT '', `sent_time` bool default null, `opened_time` varchar(30) NOT NULL DEFAULT '',  `ip` varchar(15) NOT NULL DEFAULT '', `status` int(1) DEFAULT '1',  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+#CREATE TABLE `users` (  `id` int(4) NOT NULL AUTO_INCREMENT,  `user_id` varchar(30) NOT NULL DEFAULT '',  `firstname` varchar(30) DEFAULT NULL,  `lastname` varchar(30) DEFAULT NULL,  `user_email` varchar(30) NOT NULL DEFAULT '',  `password` varchar(30) NOT NULL DEFAULT '',  `status` int(1) DEFAULT '1',  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,  PRIMARY KEY (`user_id`),  KEY `id` (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1; 
+#CREATE TABLE `addresses` (  `id` int(4) NOT NULL AUTO_INCREMENT,  `user_id` varchar(30) NOT NULL DEFAULT '',  `to_email` varchar(30) NOT NULL DEFAULT '',  `status` int(1) DEFAULT '1',  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,  PRIMARY KEY (`user_id`, `to_email`),  KEY `id` (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1; 
+
+#insert into `users` (user_id, user_email, password) values ('avs123456799', 'avs2904@webgenie.com', 'test12345');
+#insert into `mails` (user_id, rand, to_address, opened_time, ip) values ('avs123456789', '123456781', 'avs_webgenie_com@me.com', 'Jun/12/2020:13:20:45', '49.195.91.51');
+
---
+++ MOC/moc.js
@@ -0,0 +1,243 @@
+  // ***************************************************************************
+  // ///////////////////////////////////////////////////////////////////////////
+  // ***************************************************************************
+  //
+  //  Site: webgenie.com
+  //   VERSION: 1.0
+  // 
+  // ***************************************************************************
+  // ///////////////////////////////////////////////////////////////////////////
+  // ***************************************************************************
+  // Functions added by AVS
+  // Copyright (c) 2011-2019 by AV Sivaprasad and WebGenie Software Pty Ltd.
+// Global variables
+var cgi = "cgi-bin/moc.cgi"; // calls users.cgi http://www.webgenie.com/GSKY/GoogleEarth/KML/Australia_Fractional_Cover_8_Day_2018-11-01.kml
+function CopyToClipBoard()
+{
+	var textBox = document.getElementById('copytext');
+	var copyButton = document.getElementById('copybutton');
+	copybutton.style.color = '#d0d0d0';
+	var text = textBox.innerHTML;
+	textBox.select();
+	document.execCommand('copy');
+	textBox.style.display = 'none';
+	showHide("copytext", "none");
+	showHide("copybutton", "none");
+	showHide("arcgis_window",'iframe',"block");
+	showHide('bbox_finder','div','none');
+	window.location.href = "#arcgis";
+}
+function Commify(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+function Monify(value)
+{
+  if (!value || value == undefined) return "0.00";
+  var str = "" + Math.round(value*100);
+  var len = str.length;
+
+  return (str=="0")?"":(str.substring(0,len-2)+"."+str.substring(len-2,len));
+}
+function CancelJob(form)
+{
+//alert(form.id);	
+	ajaxFunction(3,form);
+}
+function ValidateInput(form,n)
+{
+	document.getElementById("img_link").innerHTML = "<img alt=\"Wait!\" src=\"/images/ajax-loader.gif\"> Fetching...&nbsp;&nbsp;&nbsp;<input type=\"button\" value=\"Cancel\" style=\"color:red\" onclick=\"CancelJob(this.form)\">";
+	ajaxFunction(n,form);
+}
+function showHideToggle(id,type)
+{
+		var style = document.getElementById(id).style.display;
+		if(style == 'none') showHide(id,type,'block');
+		else showHide(id,type,'none');
+}
+function showHide(id,type,state)
+{
+        if (state == undefined) state = 'block';
+		var style = document.getElementById(id).style.display;
+		document.getElementById(id).style.display=""+state;
+}
+function InsertAddress(form,item)
+{
+//alert(item[item.selectedIndex].value);	
+	form.to_address.value = item[item.selectedIndex].value;
+	item.selectedIndex = 0;
+}
+function CSWriteCookie() 
+{
+//	CSCookieArray = new Object;
+	var name   = "MailOpened";
+	cookieVal += "; path=/";
+	var this_cookie = name + "=" + cookieVal;
+	this_cookie += "; expires=" + "Thursday, 31-Dec-2099 00:00:00 GMT";
+	document.cookie = this_cookie;
+	var cookies = document.cookie;
+}
+function CSDeleteCookie() 
+{
+//	CSCookieArray = new Object;
+	var name   = "MailOpened";
+	cookieVal += "; path=/";
+	var this_cookie = name + "=" + cookieVal;
+	this_cookie += "; expires=" + "Thursday, 31-Dec-2099 00:00:00 GMT";
+	document.cookie = this_cookie;
+	var cookies = document.cookie;
+}
+function CSReadCookie() 
+{
+	cookieVal = '';
+	username = '';
+	var name    = "MailOpened";
+	var cookies = document.cookie;
+	var start = cookies.indexOf(name);
+//	var mobile = detectmob();
+	if(start == -1) 
+	{
+//		var loggedIn = 0
+		document.forms.login_form.User_email.value = '';
+		document.forms.login_form.passwd.value = '';
+		showHide('LoginBlock','div','block');
+		showHide('LoggedinBlock','div','none');
+//		return loggedIn;
+	}
+	else
+	{
+		start += name.length + 1;
+		var end = cookies.indexOf(";", start);
+		if(end == -1) end = cookies.length;
+		cookieVal = cookies.substring(start, end);
+		if (!cookieVal) 
+		{
+			showHide('LoginBlock','div','block');
+			showHide('LoggedinBlock','div','none');
+		}
+		else
+		{
+			cookieVal = unescape(cookieVal);
+			next = cookieVal.indexOf("|", 0);
+			User_email = unescape(cookieVal.substring(0, next));
+			start = next+1;
+			next = cookieVal.indexOf("|", start);
+			user_id = cookieVal.substring(start, next);
+			showHide('LoginBlock','none');
+			showHide('LoggedinBlock','block');
+		}
+	}
+
+	var blankcookie = cookies.indexOf(name+";");
+	if (blankcookie > 0) return "";
+	return cookieVal;
+}	
+function LoginCheckForAjax()
+{
+	CSReadCookie();
+}
+
+function LogOut()
+{
+	CSDeleteCookie();	
+	showHide('LoginBlock','div','block');
+	showHide('LoggedinBlock','div','none');
+}
+function ajaxFunction(n,form,item)
+{
+  var xmlHttp;
+  var url;
+  try
+  {  // Firefox, Opera 8.0+, Safari  
+	  xmlHttp=new XMLHttpRequest();  
+  }
+  catch (e)
+  {  // Internet Explorer  
+    try
+    {    
+		xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");    
+	}
+	catch (e)
+    {    
+		try
+		{      
+			xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");      
+		}
+		catch (e)
+      	{	      
+			alert("Your browser does not support AJAX!");      
+			return false;      
+		}    
+	}
+  }
+	xmlHttp.onreadystatechange=function()
+	{
+//alert(xmlHttp.readyState);
+	  if(xmlHttp.readyState==4)
+	  {
+ 		  response = xmlHttp.responseText;
+		  if (n == 1) // Process the response
+		  {
+		  	var fields = response.split("?");
+		  	var img_url = fields[0];
+//			var img_href = "<a href=\"" + response + "\">" + response + "</a>";
+//			document.getElementById("img_link").innerHTML = img_href;
+			document.getElementById("copytext").innerHTML = response;
+			document.getElementById("show_img").src = img_url;
+			showHide("img_link", 'span', "none");
+			showHide("copytext", 'textarea', "block");
+			copybutton.style.color = '#0000FF';
+			showHide("copybutton", 'span', "block");
+		  }
+		  if (n == 9) // login
+		  {
+//alert("9. response = " + response);
+			if (response)
+			{
+				user_id = response;
+				showHide('LoginBlock','none');
+				showHide('LoggedinBlock','block');
+				cookieVal = User_email + "|" + user_id;
+				CSWriteCookie();
+			}
+			else
+			{
+				alert('Email and/or Password is incorrect.');
+			}
+		  }
+	  }
+	}
+	if (n == 1) // Send the URL
+	{
+		showHide("img_link", 'span', "block");
+		showHide("copytext", 'textarea', "none");
+		showHide("copybutton", 'span', "none");
+		pquery = 
+		"to_address=" + form.to_address.value;
+		pquery = escape(pquery);
+		pquery = pquery.replace("+","%2B");
+		var ran_number= Math.random()*5000;
+		url = cgi + "?MOC+" + ran_number + "+" + pquery;
+	}
+	if (n == 9) // login
+	{
+		var ran_number= Math.random()*5000;
+		User_email = form.User_email.value;
+		Password = form.Password.value;
+		var ran_number= Math.random()*5000;
+		url = cgi + "?login+" + ran_number + "+" + User_email + "+" + Password;
+	}
+//alert('n = ' + n + ' url = ' + url);	
+//return;
+	if (url)
+	{
+		xmlHttp.open("GET",url,true);
+		xmlHttp.send(null);  
+	}
+	else
+	{
+		return;
+	}
+}
+
